edce007b9dd168dd80b2ac1cd00d94002c83c05d
Daniel Hess <dan9186@gmail.com>
2015-03-25 01:48:33 -0700

    adding ability to set a return error

--- mockwriter.go
+++ mockwriter.go
@@ -3,10 +3,23 @@ package mockwriter
 import ()
 
 type MockWriter struct {
-	Written []byte
+	Written     []byte
+	returnError error
+}
+
+func New() *MockWriter {
+	return &MockWriter{}
+}
+
+func (m *MockWriter) ReturnError(err error) {
+	m.returnError = err
 }
 
 func (m *MockWriter) Write(p []byte) (n int, err error) {
+	if m.returnError != nil {
+		return 0, m.returnError
+	}
+
 	m.Written = p
 	return len(m.Written), nil
 }
